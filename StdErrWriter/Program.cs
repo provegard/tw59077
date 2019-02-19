0d315497677d5570ecf6fe1081aea3f3bd2e3698
Per Rovegård <per@rovegard.se>
2019-02-19 22:42:44 +0100

    StdErrWriter

---
+++ StdErrWriter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StdErrWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Error.WriteLine("VALIDATION ERRORS:");
+        }
+    }
+}
